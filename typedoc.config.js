b9c01e69545c57b89842886d67b69c8da2d02b1f
Andrey Bronnikov <andreyb@digitalpersona.com>
2019-03-13 01:58:17 -0700

    * linting and typedoc configuration

--- typedoc.config.js
+++ typedoc.config.js
@@ -2,12 +2,7 @@ const path = require('path')
 const fs = require('fs')
 
 const excludes = [
-  { dir: 'faceLandmarkNet', exceptions: ['index.ts', 'FaceLandmark68Net.ts', 'FaceLandmark68TinyNet.ts'] },
-  { dir: 'faceRecognitionNet', exceptions: ['index.ts', 'FaceRecognitionNet.ts'] },
-  { dir: 'mtcnn', exceptions: ['index.ts', 'Mtcnn.ts', 'MtcnnOptions.ts'] },
-  { dir: 'ssdMobilenetv1', exceptions: ['index.ts', 'SsdMobilenetv1.ts', 'SsdMobilenetv1Options.ts'] },
-  { dir: 'tinyFaceDetector', exceptions: ['index.ts', 'TinyFaceDetector.ts', 'TinyFaceDetectorOptions.ts'] },
-  { dir: 'tinyYolov2', exceptions: ['index.ts', 'TinyYolov2.ts'] }
+    // { dir: '', exceptions: [ ... ] }
 ]
 
 const exclude = excludes.map(({ dir, exceptions }) => {
@@ -28,6 +23,6 @@ module.exports = {
   excludePrivate: true,
   excludeNotExported: true,
   stripInternal: true,
-  externalPattern: 'node_modules/@tensorflow',
+//  externalPattern: 'node_modules/@tensorflow',
   exclude
 }
